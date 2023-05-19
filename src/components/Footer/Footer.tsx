c98cbebf7cb956150dc62e71eabcd253aa226523
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-05-19 19:42:39 +0300

    removed unnecessary code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,21 @@
+import { border } from "../../styles/stylevariables";
 import styled from "styled-components";
 
-type Props = {};
-
-const Footer = (props: Props) => {
-  return <Container>Footer</Container>;
+const Footer = () => {
+  return (
+    <Container>
+      <Socials></Socials>
+      <p>guoda.arlauskaite@gmail.com</p>
+    </Container>
+  );
 };
 
 export default Footer;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 3vw;
+`;
+
+const Socials = styled.div``;
