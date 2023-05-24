1233c87ae3e6f012c1fed083a6e8177f8ef8c7f1
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-05-24 20:36:11 +0300

    added scroll to ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,32 @@
-import styled, { ThemeProvider } from "styled-components";
-
-import Footer from "./components/Footer/Footer";
-import GlobalStyle from "./global";
-import SideBar from "./components/SideBar/SideBar";
-import TopBar from "./components/TopBar/TopBar";
-import { theme } from "./styles/theme";
-import { useState } from "react";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Layout from "./layout/Layout";
+import Projects from "./pages/Projects/Projects";
+import Skills from "./pages/Skills/Skills";
+import { useRef } from "react";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => setIsDarkMode(!isDarkMode);
+  const mainRef = useRef<null | HTMLElement>(null);
+  const aboutRef = useRef<null | HTMLElement>(null);
+  const skillsRef = useRef<null | HTMLElement>(null);
+  const projectsRef = useRef<null | HTMLElement>(null);
+  const contactRef = useRef<null | HTMLElement>(null);
 
   return (
-    <ThemeProvider theme={isDarkMode ? theme.dark : theme.light}>
-      <Container>
-        <GlobalStyle />
-        <header>
-          <TopBar toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-        </header>
-        <Main>
-          <SideBar />
-          <Content></Content>
-        </Main>
-      </Container>
-    </ThemeProvider>
+    <Layout
+      aboutRef={aboutRef}
+      skillsRef={skillsRef}
+      projectsRef={projectsRef}
+      contactRef={contactRef}
+    >
+      <Home mainRef={mainRef} />
+      <About aboutRef={aboutRef} />
+      <Skills skillsRef={skillsRef} />
+      <Projects projectsRef={projectsRef} />
+      <Contact contactRef={contactRef} />
+    </Layout>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 100vh;
-  margin: 0 3vw;
-`;
-
-const Main = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: row-reverse;
-`;
-
-const Content = styled.div``;
