e3779112a282416b7e06ac14b2149c2b943e535a
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-06-18 16:05:17 +0300

    added contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,14 +1,47 @@
-import SectionHeader from "../../components/Headers/SectionHeader";
+import Button from "../../components/Button/Button";
+import ContactForm from "./ContactForm";
+import { roundedBorder } from "../../styles/stylevariables";
 import styled from "styled-components";
+import { useState } from "react";
 
 type Props = {
   contactRef: React.RefObject<HTMLElement>;
 };
 
 const Contact = ({ contactRef }: Props) => {
+  const [contactFormOpen, setContactFormOpen] = useState(false);
+  const [toggleButtonVisibile, setToggleButtonVisible] = useState(true);
+  const [thankYouMsg, setThankYouMsg] = useState(false);
+
+  const handleAfterSubmit = () => {
+    setContactFormOpen(false);
+    setThankYouMsg(true);
+    setTimeout(() => {
+      setToggleButtonVisible(true);
+      setThankYouMsg(false);
+    }, 3000);
+  };
+
+  const toggleFormOpen = () => {
+    setContactFormOpen(!contactFormOpen);
+    setToggleButtonVisible(!toggleButtonVisibile);
+  };
+
   return (
     <Container ref={contactRef}>
-      <SectionHeader>Contact</SectionHeader>
+      <TextWrapper>
+        {thankYouMsg ? (
+          <Text>Thank you! I'll get back to you soon.</Text>
+        ) : (
+          <Text>Want to work together or have any questions?</Text>
+        )}
+        {toggleButtonVisibile ? (
+          <Button onClick={toggleFormOpen} large>
+            Get in Touch
+          </Button>
+        ) : null}
+      </TextWrapper>
+      {contactFormOpen ? <ContactForm closeForm={handleAfterSubmit} /> : null}
     </Container>
   );
 };
@@ -16,5 +49,15 @@ const Contact = ({ contactRef }: Props) => {
 export default Contact;
 
 const Container = styled.section`
-  height: 100vh;
+  border-radius: ${roundedBorder};
+  background-color: ${({ theme }) => theme.secondary};
+  padding: 20px 10vw;
+`;
+
+const TextWrapper = styled.div`
+  text-align: center;
+`;
+
+const Text = styled.p`
+  font-size: 2rem;
 `;
